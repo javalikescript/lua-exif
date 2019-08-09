cc0c46ef1439fc2f57eb32b919d3a462d0cd57e1
javalikescript <javalikescript@free.fr>
2019-08-09 22:29:31 +0200

    first commit

---
+++ exif.c
@@ -0,0 +1,427 @@
+//#define JLS_LUA_MOD_TRACE 1
+#include "luamod.h"
+
+#include <libexif/exif-loader.h>
+
+/*
+********************************************************************************
+* EXIF Structures
+********************************************************************************
+*/
+
+typedef struct LuaExifLoaderStruct {
+	ExifLoader *exifLoader;
+} LuaExifLoader;
+
+typedef struct LuaExifDataStruct {
+	ExifData *exifData;
+} LuaExifData;
+
+#define COMPONENTS_PROPERTY_NAME "components"
+#define FORMAT_PROPERTY_NAME     "format"
+#define SIZE_PROPERTY_NAME       "size"
+#define TAG_PROPERTY_NAME        "tag"
+#define VALUE_PROPERTY_NAME      "value"
+#define DATA_PROPERTY_NAME       "data"
+
+/*
+********************************************************************************
+* Helper functions
+********************************************************************************
+*/
+
+static const char *toBuffer(lua_State *l, int i, size_t *len) {
+	if (lua_isstring(l, i)) {
+		return lua_tolstring(l, i, len);
+	} else if (lua_isuserdata(l, i) && !lua_islightuserdata(l, i)) {
+		*len = lua_rawlen(l, i);
+		return (const char *)lua_touserdata(l, i);
+	}
+	return NULL;
+}
+
+static const char *checkBuffer(lua_State *l, int i, size_t *len) {
+	const char *buffer = toBuffer(l, i, len);
+	if (buffer == NULL) {
+		luaL_argerror(l, i, "string or userdata expected");
+	}
+	return buffer;
+}
+
+/*
+********************************************************************************
+* EXIF Data functions
+********************************************************************************
+*/
+
+static int luaexif_data_new(lua_State *l) {
+	trace("luaexif_data_new()\n");
+	LuaExifData *data = (LuaExifData *)lua_newuserdata(l, sizeof(LuaExifData));
+	data->exifData = exif_data_new();
+	luaL_getmetatable(l, "exif_data");
+	lua_setmetatable(l, -2);
+	return 1;
+}
+
+static int luaexif_data_load(lua_State *l) {
+	trace("luaexif_data_load()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+	size_t length;
+	const char *buffer = checkBuffer(l, 2, &length);
+	exif_data_load_data(data->exifData, buffer, length);
+	return 0;
+}
+
+static int luaexif_data_save(lua_State *l) {
+	trace("luaexif_data_save()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+	unsigned char *d = NULL;
+	unsigned int ds = 0;
+	exif_data_save_data(data->exifData, &d, &ds);
+	if (d == NULL) {
+		return 0;
+	}
+	lua_pushlstring(l, d, (size_t)ds);
+	free(d);
+	return 0;
+}
+
+static int luaexif_data_get_byte_order(lua_State *l) {
+	trace("luaexif_data_get_byte_order()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+	ExifByteOrder bo = exif_data_get_byte_order(data->exifData);
+	lua_pushinteger(l, (lua_Integer) bo);
+	return 1;
+}
+
+static int luaexif_data_set_byte_order(lua_State *l) {
+	trace("luaexif_data_set_byte_order()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+	ExifByteOrder bo = (ExifByteOrder) luaL_checkinteger(l, 2);
+	exif_data_set_byte_order(data->exifData, bo);
+	return 0;
+}
+
+static int luaexif_data_fix(lua_State *l) {
+	trace("luaexif_data_fix()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+	exif_data_fix(data->exifData);
+	return 0;
+}
+
+static int luaexif_data_from_table(lua_State *l) {
+	trace("luaexif_data_from_table()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+
+	luaL_checktype(l, 2, LUA_TTABLE);
+
+	ExifByteOrder bo = exif_data_get_byte_order(data->exifData);
+    ExifIfd ifd;
+    for (ifd = EXIF_IFD_0; ifd < EXIF_IFD_COUNT; ifd++) {
+    	trace("look for %s (%d)\n", exif_ifd_get_name(ifd), ifd);
+		lua_getfield(l, 2, exif_ifd_get_name(ifd));
+		if (lua_istable(l, -1)) {
+			trace("found \"%s\"\n", exif_ifd_get_name(ifd));
+			lua_pushnil(l);
+			while (lua_next(l, -2)) {
+				if (lua_isstring(l, -2) && lua_istable(l, -1)) {
+					ExifTag entryTag = 0;
+					ExifFormat entryFormat = 0;
+					unsigned long entryComponents = 0;
+					//unsigned char *data;
+					unsigned int entrySize = 0;
+					const char *entryValue = "";
+					int entryDataLength = -1;
+					const char *entryName = lua_tostring(l, -2);
+					trace("entry \"%s\"\n", entryName);
+					SET_OPT_INTEGER_FIELD(l, -1, entryComponents, COMPONENTS_PROPERTY_NAME);
+					SET_OPT_INTEGER_FIELD(l, -1, entryFormat, FORMAT_PROPERTY_NAME);
+					SET_OPT_INTEGER_FIELD(l, -1, entrySize, SIZE_PROPERTY_NAME);
+					SET_OPT_INTEGER_FIELD(l, -1, entryTag, TAG_PROPERTY_NAME);
+					SET_OPT_STRING_FIELD(l, -1, entryValue, VALUE_PROPERTY_NAME);
+					lua_getfield(l, -1, DATA_PROPERTY_NAME);
+					if (lua_istable(l, -1)) { \
+						lua_len(l, -1);
+						entryDataLength = lua_tointeger(l, -1);
+						lua_pop(l, 1);
+					}
+					lua_pop(l, 1);
+		        	trace(" - tag: %d, format: %d, components: %d (#%d), size: %d value: \"%s\"\n",
+						entryTag, entryFormat, entryComponents, entryDataLength, entrySize, entryValue);
+					if ((entryTag == 0) || (entryFormat == 0) || ((entryDataLength >= 0) && (entryComponents != entryDataLength))) {
+						trace("skipping invalid entry\n");
+						lua_pop(l, 1);
+						continue;
+					}
+					void *buf;
+					ExifEntry *entry;
+					ExifMem *mem = exif_mem_new_default();
+					entry = exif_entry_new_mem(mem);
+					buf = exif_mem_alloc(mem, entrySize);
+					entry->data = buf;
+					entry->size = entrySize;
+					entry->tag = entryTag;
+					entry->components = entryComponents;
+					entry->format = entryFormat;
+					exif_content_add_entry(data->exifData->ifd[ifd], entry);
+					exif_mem_unref(mem);
+					exif_entry_unref(entry);
+					if (entryFormat == EXIF_FORMAT_ASCII) {
+						memcpy(entry->data, entryValue, strlen(entryValue));
+					} else if (entryDataLength > 0) {
+						lua_getfield(l, -1, DATA_PROPERTY_NAME);
+						trace("process %d component(s)\n", entryDataLength);
+						int fs = exif_format_get_size(entryFormat);
+						int i, offset = 0;
+						for (i = 1; i <= entryDataLength; i++) {
+							lua_geti(l, -1, i);
+							double d = lua_tonumber(l, -1);
+							lua_pop(l, 1);
+							trace("data[%d] = %f\n", i, (long) d);
+							switch (entryFormat) {
+							case EXIF_FORMAT_BYTE:
+								entry->data[offset] = (ExifByte) d;
+								break;
+							case EXIF_FORMAT_SBYTE:
+								entry->data[offset] = (ExifSByte) d;
+								break;
+							case EXIF_FORMAT_SHORT:
+								exif_set_short(entry->data + offset, bo, (ExifShort) d);
+								break;
+							case EXIF_FORMAT_SSHORT:
+								exif_set_sshort(entry->data + offset, bo, (ExifSShort) d);
+								break;
+							case EXIF_FORMAT_LONG:
+								exif_set_long(entry->data + offset, bo, (ExifLong) d);
+								break;
+							case EXIF_FORMAT_SLONG:
+								exif_set_slong(entry->data + offset, bo, (ExifSLong) d);
+								break;
+							case EXIF_FORMAT_DOUBLE:
+								//exif_set_do(entry->data + offset, bo);
+								break;
+							case EXIF_FORMAT_RATIONAL:
+								//exif_set_rational(entry->data + offset, bo);
+								break;
+							case EXIF_FORMAT_SRATIONAL:
+								//exif_set_srational(entry->data + offset, bo);
+								break;
+							case EXIF_FORMAT_FLOAT:
+							case EXIF_FORMAT_UNDEFINED:
+								break;
+							}
+							offset += fs;
+						}
+						lua_pop(l, 1);
+					}
+
+				}
+				lua_pop(l, 1);
+			}
+		}
+		lua_pop(l, 1);
+	}
+
+	return 0;
+}
+
+static int luaexif_data_to_table(lua_State *l) {
+	trace("luaexif_data_to_table()\n");
+	LuaExifData *data = (LuaExifData *)luaL_checkudata(l, 1, "exif_data");
+	// table map with an item per EXIF IFD
+	lua_newtable(l);
+	ExifByteOrder bo = exif_data_get_byte_order(data->exifData);
+    ExifIfd ifd;
+    for (ifd = EXIF_IFD_0; ifd < EXIF_IFD_COUNT; ifd++) {
+    	ExifContent *content = data->exifData->ifd[ifd];
+    	trace("addAllEntries(%d, \"%s\"): %d entries to add\n", ifd, exif_ifd_get_name(ifd), content->count);
+		lua_pushstring(l, exif_ifd_get_name(ifd));
+		// table map with an item per content
+		lua_newtable(l);
+    	int i;
+    	for (i = 0; i < content->count; i++) {
+    		ExifEntry *entry = content->entries[i];
+    		char value[1024];
+    		/* Get the contents of the tag in human-readable form */
+    		exif_entry_get_value(entry, value, sizeof(value));
+    		//trimSpaces(value);
+    		if (*value) {
+                const char *name = exif_tag_get_name_in_ifd(entry->tag, ifd);
+            	trace("- entry \"%s\" tag: %d, format: %d, c: %d, s: %d value: \"%s\"\n", name, entry->tag, entry->format, entry->components, entry->size, value);
+				lua_pushstring(l, name);
+				// table map with an item per entry
+				lua_newtable(l);
+				SET_TABLE_KEY_INTEGER(l, COMPONENTS_PROPERTY_NAME, entry->components);
+				SET_TABLE_KEY_INTEGER(l, FORMAT_PROPERTY_NAME, entry->format);
+				SET_TABLE_KEY_INTEGER(l, SIZE_PROPERTY_NAME, entry->size);
+				SET_TABLE_KEY_INTEGER(l, TAG_PROPERTY_NAME, entry->tag);
+				SET_TABLE_KEY_STRING(l, VALUE_PROPERTY_NAME, value);
+            	if (entry->format != EXIF_FORMAT_ASCII) {
+					lua_pushstring(l, DATA_PROPERTY_NAME);
+					// table list with an item per component
+					lua_newtable(l);
+					int fs = exif_format_get_size(entry->format);
+					int i, offset = 0;
+					for (i = 1; i <= entry->components; i++) {
+						lua_Number d = 0.0;
+						switch (entry->format) {
+						case EXIF_FORMAT_BYTE:
+							d = (ExifByte) entry->data[offset];
+							break;
+						case EXIF_FORMAT_SBYTE:
+							d = (ExifSByte) entry->data[offset];
+							break;
+						case EXIF_FORMAT_SHORT:
+							d = exif_get_short(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_SSHORT:
+							d = exif_get_sshort(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_LONG:
+							d = exif_get_long(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_SLONG:
+							d = exif_get_slong(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_DOUBLE:
+							//d = exif_get_do(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_RATIONAL:
+							//exif_get_rational(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_SRATIONAL:
+							//exif_get_srational(entry->data + offset, bo);
+							break;
+						case EXIF_FORMAT_FLOAT:
+						case EXIF_FORMAT_UNDEFINED:
+							break;
+						}
+						trace("data[%d] = %f\n", i, d);
+						lua_pushnumber(l, d);
+						lua_rawseti(l, -2, i); // add value in data table list
+						offset += fs;
+					}
+					lua_rawset(l, -3); // add the data list
+            	}
+				lua_rawset(l, -3); // add the entry map
+			}
+		}
+		lua_rawset(l, -3); // add the ifd map
+	}
+	return 1;
+}
+
+static int luaexif_data_gc(lua_State *l) {
+	trace("luaexif_data_gc()\n");
+	LuaExifData *data = (LuaExifData *)luaL_testudata(l, 1, "exif_data");
+	if ((data != NULL) && (data->exifData != NULL)) {
+		exif_data_unref(data->exifData);
+	}
+	return 0;
+}
+
+
+/*
+********************************************************************************
+* EXIF Loader functions
+********************************************************************************
+*/
+
+static int luaexif_loader_new(lua_State *l) {
+	trace("luaexif_loader_new()\n");
+	LuaExifLoader *loader = (LuaExifLoader *)lua_newuserdata(l, sizeof(LuaExifLoader));
+	loader->exifLoader = exif_loader_new();
+	luaL_getmetatable(l, "exif_loader");
+	lua_setmetatable(l, -2);
+	return 1;
+}
+
+static int luaexif_loader_write(lua_State *l) {
+	trace("luaexif_loader_write()\n");
+	size_t bufferLength = 0;
+	unsigned char *bufferData = NULL;
+	LuaExifLoader *loader = (LuaExifLoader *)luaL_checkudata(l, 1, "exif_loader");
+
+	if (lua_isstring(l, 2)) {
+		bufferData = (unsigned char *)luaL_checklstring(l, 2, &bufferLength);
+	} else {
+		luaL_checktype(l, 2, LUA_TUSERDATA);
+		bufferLength = lua_rawlen(l, 2);
+		bufferData = (unsigned char *)lua_touserdata(l, 2);
+	}
+	// return 1 while EXIF data is read (or while there is still hope that there will be EXIF data later on), 0 otherwise.
+	int result = exif_loader_write(loader->exifLoader, bufferData, bufferLength);
+
+	lua_pushboolean(l, result);
+	return 1;
+}
+
+static int luaexif_loader_get_data(lua_State *l) {
+	trace("luaexif_loader_get_data()\n");
+	LuaExifLoader *loader = (LuaExifLoader *)luaL_checkudata(l, 1, "exif_loader");
+
+	ExifData *exifData = exif_loader_get_data(loader->exifLoader);
+	if (exifData == NULL) {
+		return 0;
+	}
+	LuaExifData *data = (LuaExifData *)lua_newuserdata(l, sizeof(LuaExifData));
+	data->exifData = exifData;
+	luaL_getmetatable(l, "exif_data");
+	lua_setmetatable(l, -2);
+	return 1;
+}
+
+static int luaexif_loader_gc(lua_State *l) {
+	trace("luaexif_loader_gc()\n");
+	LuaExifLoader *loader = (LuaExifLoader *)luaL_testudata(l, 1, "exif_loader");
+	if ((loader != NULL) && (loader->exifLoader != NULL)) {
+		exif_loader_unref(loader->exifLoader);
+	}
+	return 0;
+}
+
+
+/*
+********************************************************************************
+* Module open function
+********************************************************************************
+*/
+
+LUALIB_API int luaopen_exif(lua_State *l) {
+	trace("luaopen_exif()\n");
+
+	luaL_newmetatable(l, "exif_data");
+	lua_pushstring(l, "__gc");
+	lua_pushcfunction(l, luaexif_data_gc);
+	lua_settable(l, -3);
+
+	luaL_newmetatable(l, "exif_loader");
+	lua_pushstring(l, "__gc");
+	lua_pushcfunction(l, luaexif_loader_gc);
+	lua_settable(l, -3);
+
+	luaL_Reg reg[] = {
+		// Data
+		{ "data_new", luaexif_data_new },
+		{ "data_load", luaexif_data_load },
+		{ "data_save", luaexif_data_save },
+		{ "data_get_byte_order", luaexif_data_get_byte_order },
+		{ "data_set_byte_order", luaexif_data_set_byte_order },
+		{ "data_from_table", luaexif_data_from_table },
+		{ "data_to_table", luaexif_data_to_table },
+		{ "data_fix", luaexif_data_fix },
+		// Loader
+		{ "loader_new", luaexif_loader_new },
+		{ "loader_write", luaexif_loader_write },
+		{ "loader_get_data", luaexif_loader_get_data },
+		{ NULL, NULL }
+	};
+	lua_newtable(l);
+	luaL_setfuncs(l, reg, 0);
+	lua_pushliteral(l, "Lua exif");
+	lua_setfield(l, -2, "_NAME");
+	lua_pushliteral(l, "0.1");
+	lua_setfield(l, -2, "_VERSION");
+	trace("luaopen_exif() done\n");
+	return 1;
+}
---
+++ sample.lua
@@ -0,0 +1,40 @@
+local bufferLib = require('buffer')
+local exifLib = require('exif')
+
+local function printTable(t, i, p)
+    i = i or '  '
+    p = p or i
+    for name, value in pairs(t) do
+        if (type(value) == 'table') then
+            print(p..tostring(name)..':')
+            printTable(value, p..i)
+        else
+            print(p..tostring(name)..' = '..tostring(value))
+        end
+    end
+end
+
+-- see https://github.com/ianare/exif-samples/tree/master/jpg
+local filename = arg[1] or 'lua-exif/Sony_HDR-HC3.jpg'
+
+local loader = exifLib.loader_new()
+
+print('reading '..filename)
+local fd = io.open(filename, 'rb')
+repeat
+    local chunk = fd:read(2048)
+until not exifLib.loader_write(loader, chunk)
+fd:close()
+
+local data = exifLib.loader_get_data(loader)
+
+if data then
+    print('EXIF:')
+    local t = exifLib.data_to_table(data)
+    printTable(t)
+
+    local newData = exifLib.data_new()
+    exifLib.data_from_table(newData, t)
+else
+    print('no EXIF data found')
+end
